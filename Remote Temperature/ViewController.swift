e4972ac16fd0a09ba021a271691d339ab6c42efe
Mike Lyons <mike.s.lyons@gmail.com>
2018-07-04 14:12:23 -0400

    Initial Commit

---
+++ Remote Temperature/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Remote Temperature
+//
+//  Created by Michael Lyons on 7/4/18.
+//  Copyright © 2018 NullCoreSystems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
